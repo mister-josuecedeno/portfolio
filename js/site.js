f0cde0650510ced317b7e0a15da05beef26f76e1
mister-josuecedeno <josuecedeno@yahoo.com>
2021-04-08 16:33:43 -0500

    Add scroll icon, Update About Me, Upate Skills

--- js/site.js
+++ js/site.js
@@ -1,20 +1,33 @@
 const btn = document.getElementById('button');
 
-document.getElementById('form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
+let myForm = document.getElementById('form');
+myForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    btn.value = 'Sending...';
+  btn.value = 'Sending...';
 
-    const serviceID = 'default_service';
-    const templateID = 'template_gotos3l';
+  const serviceID = 'default_service';
+  const templateID = 'template_gotos3l';
 
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-      }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-      });
-  });
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      // alert('Sent!');
+      Swal.fire({
+        title: 'Email Sent',
+        text: 'Your email was sent successfully. I will respond within 24 hours.',
+        icon: 'success',
+        confirmButtonText: 'Excellent!'
+      })
+      myForm.reset();
+    }, (err) => {
+      btn.value = 'Send Email';
+      Swal.fire({
+        title: 'Error!',
+        html: 'Looks like something went wrong! <hr /> I\'m always available on <a href="https://www.linkedin.com/in/josuecedeno/" target="_blank">LinkedIn</a>',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      })
+      // alert(JSON.stringify(err));
+    });
+});
